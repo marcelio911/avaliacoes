abe36aabc6b1458bf08358f2738c505e818342c3
marcelio911 <marcelio911@gmail.com>
2019-06-14 15:18:19 -0300

    removendo pasta de teste

--- AwesomeProject/assets/static.js
+++
@@ -1,7 +0,0 @@
-const images = {
-	menuIcon: require('./imagens/produto.jpg'),
-	profileIcon: require('/img/profile-icon.png'),
-	settingsIcon: require('./img/settings-icon.png'),
-};
-
-export default images;
