cbdb36431cd525c87a391dd87083a9b65cd972b3
marcelio911 <marcelio911@gmail.com>
2019-06-14 16:25:58 -0300

    inserindo imagens dos produtos

--- prova-api/src/main/resources/data.sql
+++ prova-api/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into "tb_produto"
-values(10001,'Calça','', 50.2);
+values(10001,'Calça','./imagens/produto.jpg', 50.2);
 
 insert into "tb_produto"
-values(10002,'Tênis', '', 145.65);
+values(10002,'Tênis', './imagens/produto2.jpg', 145.65);
 
 insert into "tb_produto"
-values(10003,'Camisa', '', 29.99);
+values(10003,'Camisa', './imagens/produto3.jpg', 29.99);
 -- UPDATE hibernate_sequence SET next_val=10003 where next_val=10002;
 
 -- INSERT INTO "tb_usuario" (ct_email, nm_usuario, sc_usuario, sc_password) VALUES ('admin@admin.com','admin','admin','admin');
